0c52fbc6e8477f500adbc1a9aa337eb09bbdd0f0
ipeng6 <ipeng6@qq.com>
2018-11-27 23:06:44 +0800

    fix el null

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ $docsify.plugins = [
   function(hook, vm) {
     hook.doneEach(function(html, next) {
       let el = document.querySelector('.sidebar-nav .active');
-      el.classList.add('open');
-      while (el && el.className !== 'sidebar-nav') {
-        if (el.parentElement.tagName === 'LI') {
-          el.parentElement.className = 'open';
+      if (el) {
+        el.classList.add('open');
+        while (el.className !== 'sidebar-nav') {
+          if (el.parentElement.tagName === 'LI') {
+            el.parentElement.className = 'open';
+          }
+          el = el.parentElement;
         }
-        el = el.parentElement;
       }
       next(html);
     });
-  },
+  }
 ].concat($docsify.plugins || []);
